ea3b618f9e7bb68801b1f489ebbd8e58615b23f4
MohdShavezz <mohdpctebtech19cse@gmail.com>
2026-04-09 11:50:19 +0530

    authApi: forgot and reset passwork nodemailer auth middlaware

--- Lec_32_Apr_07_authApis/routes/user.js
+++ Lec_32_Apr_07_authApis/routes/user.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { getUserById, getUsers, login, register } from '../controller/user.js'
+import { forgotPassword, getUserById, getUsers, login, register } from '../controller/user.js'
 
 const router=express.Router()
 
 
 router.route('/register').post(register)   //http://localhost:3000/api/user/register POST
 router.route('/login').post(login)   //http://localhost:3000/api/user/login POST
+router.route('/forgot-password').post(forgotPassword)   //http://localhost:3000/api/user/forgot-password POST {email}
+router.route('/reset-password').post(resetPassword)   //http://localhost:3000/api/user/reset-password POST {newpassword,token}
 router.route('/users').get(getUsers)   //http://localhost:3000/api/user/users GET
 router.route('/:id').get(getUserById)   //http://localhost:3000/api/user {id} GET
 
